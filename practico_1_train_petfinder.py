15c886dd83c1359f26a645a49d516990593a0a06
Fernando E. Mancuso <fernando_mancuso@mcafee.com>
2020-01-13 12:41:15 -0300

    update TP

--- practico_1_train_petfinder.py
+++ practico_1_train_petfinder.py
@@ -18,7 +18,9 @@ import pandas
 import tensorflow as tf
 
 from sklearn.model_selection import train_test_split
-from tensorflow.keras import layers, models, Sequential
+from tensorflow.keras import layers, models
+from sklearn.utils import shuffle
+from itertools import zip_longest
 
 TARGET_COL = 'AdoptionSpeed'
 
@@ -40,6 +42,10 @@ def read_args():
                         help='Number of instances in each batch.')
     parser.add_argument('--experiment_name', type=str, default='Base model',
                         help='Name of the experiment, used in mlflow.')
+    parser.add_argument('--one_hot_columns', nargs='+', type=str, default=['Gender', 'Color1'],
+                        help='Name of column to be one hot encoded.')
+    parser.add_argument('--embedding_columns', nargs='+', type=str, default=['Breed1'],
+                        help='Name of columns to be embedded.')
     args = parser.parse_args()
 
     assert len(args.hidden_layer_sizes) == len(args.dropout)
@@ -48,7 +54,6 @@ def read_args():
 
 def process_features(df, one_hot_columns, numeric_columns, embedded_columns, test=False):
     direct_features = []
-    numeric_features = []
 
     # Create one hot encodings
     for one_hot_col, max_value in one_hot_columns.items():
@@ -56,16 +61,10 @@ def process_features(df, one_hot_columns, numeric_columns, embedded_columns, tes
 
     # Create and append numeric columns
 
-    for numeric_col in numeric_columns:
-        numeric_features.append(tf.keras.utils.normalize(df[numeric_col].to_numpy()))
-
-    # Concatenate all features that don't need further embedding into a single matrix.
-    features = {'direct_features': numpy.hstack(direct_features),
-                'numeric_features': numpy.asarray(numeric_features)}
-
-    x_shape = features['direct_features'].shape[0]
-    size = features['numeric_features'].size
-    features['numeric_features'] = features['numeric_features'].reshape(x_shape, int(size / x_shape))
+    for numeric_column in numeric_columns:
+        direct_features.append(tf.keras.utils.normalize(df[numeric_column].values.reshape(-1,1)))
+    
+    features = {'direct_features': numpy.hstack(direct_features)}
 
     # Create embedding columns - nothing to do here. We will use the zero embedding for OOV
     for embedded_col in embedded_columns.keys():
@@ -94,119 +93,114 @@ def load_dataset(dataset_dir, batch_size):
     return dataset, dev_dataset, test_dataset
 
 
-def main():
-    args = read_args()
-    dataset, dev_dataset, test_dataset = load_dataset(args.dataset_dir, args.batch_size)
-    nlabels = dataset[TARGET_COL].unique().shape[0]
+def build_model(embedded_columns, direct_features_input, direct_features_input_shape,
+               hidden_layer_sizes, dropouts, n_labels):
+    tf.keras.backend.clear_session()
 
-    # It's important to always use the same one-hot length
-    one_hot_columns = {
-        one_hot_col: dataset[one_hot_col].max()
-        for one_hot_col in ['Gender', 'Color1']
-    }
-    embedded_columns = {
-        embedded_col: dataset[embedded_col].max() + 1
-        for embedded_col in ['Breed1']
-    }
-    numeric_columns = {
-        numeric_col: dataset[numeric_col].max() + 1
-        for numeric_col in ['Age', 'Fee']
-    }
-
-    # TODO (optional) put these three types of columns in the same dictionary with "column types"
-    X_train, y_train = process_features(dataset, one_hot_columns, numeric_columns, embedded_columns)
-    direct_features_input_shape = (X_train['direct_features'].shape[1],)
-    X_dev, y_dev = process_features(dev_dataset, one_hot_columns, numeric_columns, embedded_columns)
-
-    # Create the tensorflow Dataset
-    batch_size = 32
-    # TODO shuffle the train dataset!
-    train_ds = tf.data.Dataset.from_tensor_slices((X_train, y_train)).batch(batch_size)
-    dev_ds = tf.data.Dataset.from_tensor_slices((X_dev, y_dev)).batch(batch_size)
-    test_ds = tf.data.Dataset.from_tensor_slices(process_features(
-        test_dataset, one_hot_columns, numeric_columns, embedded_columns, test=True)[0]).batch(batch_size)
-
-    # TODO: Build the Keras model
     embedding_layers = []
-    numeric_layers = []
     inputs = []
     for embedded_col, max_value in embedded_columns.items():
         input_layer = layers.Input(shape=(1,), name=embedded_col)
         inputs.append(input_layer)
-        # Define the embedding layer
         embedding_size = int(max_value / 4)
         embedding_layers.append(
             tf.squeeze(layers.Embedding(input_dim=max_value, output_dim=embedding_size)(input_layer), axis=-2))
         print('Adding embedding of size {} for layer {}'.format(embedding_size, embedded_col))
 
-    hidden_layer_size = 64
-
     direct_features_input = layers.Input(shape=direct_features_input_shape, name='direct_features')
+    inputs.append(direct_features_input)
 
-    for numeric_col, max_value in numeric_columns.items():
-        input_layer = layers.Input(shape=(1,), name=numeric_col)
-        inputs.append(input_layer)
-        # Define the numeric layer
-        numeric_size = int(max_value / 4)
-        numeric_layers.append(input_layer)
-        print('Adding numeric of size {} for layer {}'.format(numeric_size, numeric_col))
+    features = layers.concatenate(embedding_layers + [direct_features_input])
 
-    # Concatenate everything together
-    features = layers.concatenate(embedding_layers + numeric_layers + [direct_features_input])
+    if len(hidden_layer_sizes) > 0:
+        hidden_layer_size = hidden_layer_sizes.pop(0)
+        last_hidden_layer = layers.Dense(hidden_layer_size, activation='relu')(features)
+    if len(dropouts) > 0:
+        dropout = dropouts.pop(0)
+        last_hidden_layer = layers.Dropout(dropout)(last_hidden_layer)
+        
+    if len(dropouts) > len(hidden_layer_sizes):
+        dropouts = dropouts[:len(hidden_layer_sizes)]
+        
+    for hidden_layer_size, dropout in zip_longest(hidden_layer_sizes, dropouts, fillvalue=None):
+        if hidden_layer_size != None:
+            last_hidden_layer = layers.Dense(hidden_layer_size, activation='relu')(last_hidden_layer)
+        if dropout != None:
+            last_hidden_layer = layers.Dropout(dropout)(last_hidden_layer)
+        
+    output_layer = layers.Dense(n_labels, activation='softmax')(last_hidden_layer)
+    return models.Model(inputs=inputs, outputs=output_layer)
 
-    dense1 = layers.Dense(hidden_layer_size, activation='relu')(features)
 
-    output_layer = layers.Dense(nlabels, activation='softmax')(dense1)
+def main():
+    args = read_args()
+    dataset, dev_dataset, test_dataset = load_dataset(args.dataset_dir, args.batch_size)
+    nlabels = dataset[TARGET_COL].unique().shape[0]
+    
+    one_hot_columns = {
+        one_hot_col: dataset[one_hot_col].max()
+        for one_hot_col in args.one_hot_columns
+    }
+    embedded_columns = {
+        embedded_col: dataset[embedded_col].max() + 1
+        for embedded_col in args.embedding_columns
+    }
+    numeric_columns = ['Age', 'Fee']
 
+    dataset = shuffle(dataset, random_state=12345)
+    
+    X_train, y_train = process_features(dataset, one_hot_columns, numeric_columns, embedded_columns)
+    direct_features_input_shape = (X_train['direct_features'].shape[1],)
+    X_dev, y_dev = process_features(dev_dataset, one_hot_columns, numeric_columns, embedded_columns)
+    
+    batch_size = args.batch_size
 
-    inputs.append(features)
+    train_ds = tf.data.Dataset.from_tensor_slices((X_train, y_train)).batch(batch_size)
+    dev_ds = tf.data.Dataset.from_tensor_slices((X_dev, y_dev)).batch(batch_size)
+    X_test, y_test = process_features(
+        test_dataset, one_hot_columns, numeric_columns, embedded_columns, test=True)
+    test_ds = tf.data.Dataset.from_tensor_slices(X_test).batch(batch_size)
 
-    model = models.Model(inputs=inputs, outputs=output_layer)
+    model = build_model(embedded_columns, X_train['direct_features'], direct_features_input_shape, 
+            args.hidden_layer_sizes,
+            args.dropout,
+            nlabels)
 
-    model.compile(loss='categorical_crossentropy',
-                  metrics=['accuracy'])
+    model.compile(loss='categorical_crossentropy', 
+                  optimizer='adam',metrics=['accuracy'])
 
-    # TODO: Fit the model
+    print(model.summary())
+
     mlflow.set_experiment(args.experiment_name)
 
     with mlflow.start_run(nested=True):
-        # Log model hiperparameters first
-        mlflow.log_param('hidden_layer_size', args.hidden_layer_sizes)
+        mlflow.log_param('hidden_layer_sizes', args.hidden_layer_sizes)
+        mlflow.log_param('dropout', args.dropout)
         mlflow.log_param('embedded_columns', embedded_columns)
         mlflow.log_param('one_hot_columns', one_hot_columns)
-        # mlflow.log_param('numerical_columns', numerical_columns)  # Not using these yet
+        mlflow.log_param('numeric_columns', numeric_columns)
         mlflow.log_param('epochs', args.epochs)
 
-        # Train
-        history = model.fit(train_ds, epochs=args.epochs)
+        history = model.fit(train_ds, epochs=args.epochs, validation_data=dev_ds, verbose=0)
 
-        # TODO: analyze history to see if model converges/overfits
-        print("HISTORY:")
-        print(history)
+        print(history.history.keys())
 
-        # TODO: Evaluate the model, calculating the metrics.
-        # Option 1: Use the model.evaluate() method. For this, the model must be
-        # already compiled with the metrics.
-        # performance = model.evaluate(X_test, y_test)
 
-        # loss, accuracy = 0, 0
+        loss, accuracy = 0, 0
         loss, accuracy = model.evaluate(dev_ds)
         print("*** Dev loss: {} - accuracy: {}".format(loss, accuracy))
-        mlflow.log_metric('loss', loss)
-        mlflow.log_metric('accuracy', accuracy)
-
-        # Option 2: Use the model.predict() method and calculate the metrics using
-        # sklearn. We recommend this, because you can store the predictions if
-        # you need more analysis later. Also, if you calculate the metrics on a
-        # notebook, then you can compare multiple classifiers.
-
-        # predictions = 'No prediction yet'
+        mlflow.log_metric('dev_loss', loss)
+        mlflow.log_metric('dev_accuracy', accuracy)
+        
+        predictions = 'No prediction yet'
         predictions = model.predict(test_ds)
 
-        # TODO: Convert predictions to classes
-        # TODO: Save the results for submission
-        # ...
+        test_dataset["AdoptionSpeed"] = predictions.argmax(axis=1)
+        test_dataset.to_csv("./submission.csv", index=False, columns=["PID", "AdoptionSpeed"])
         print(predictions)
+
+        
+print('All operations completed')
 
 
 print('All operations completed')
